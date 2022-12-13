97c7db07698df3ba22da5a8dd861ac2ff3d6705a
Deiontre10 <travontazlowry@gmail.com>
2022-12-13 18:42:08 -0500

    updated index.html trying to configure the rows and columns

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
 var lat = "35.227085";
 var lon = "-80.843124";
 var apiKey = "06d1869c73d270a477b863d31ffacc7e"
 var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
 
-$(function (){
-    
-    fetch(weatherUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+$(function () {
 
+  fetch(weatherUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  
 
 });
