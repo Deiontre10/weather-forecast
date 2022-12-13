f30fb9ace508079d784e52a9d3989e790596c8b6
Deiontre10 <travontazlowry@gmail.com>
2022-12-13 09:52:11 -0500

    added test lat and lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-var lat;
-var lon;
-var weatherUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={06d1869c73d270a477b863d31ffacc7e}";
+var lat = "35.227085";
+var lon = "-80.843124";
+var apiKey = "06d1869c73d270a477b863d31ffacc7e"
+var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
 
 $(function (){
     
