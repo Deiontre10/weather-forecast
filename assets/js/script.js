6700d196a8fda9ea918200b2a8ab4a257a6ad302
Deiontre10 <travontazlowry@gmail.com>
2022-12-12 20:07:37 -0500

    started to work on script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-console.log("hello");
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+var weatherUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={06d1869c73d270a477b863d31ffacc7e}";
+
+$(function (){
+    
+    fetch(weatherUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
+});
