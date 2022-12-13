7543c16dc3e5a390b4a9cfd2fe52f166f9865d70
Deiontre10 <travontazlowry@gmail.com>
2022-12-12 20:55:12 -0500

    setup variables in script.js'
    '

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+var lat;
+var lon;
 var weatherUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={06d1869c73d270a477b863d31ffacc7e}";
 
 $(function (){
