905aebf1d94f25f2858645c925953740be6c8277
Deiontre10 <travontazlowry@gmail.com>
2022-12-15 20:53:56 -0500

    got data onto the page but my date dissappears

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,16 @@ var cityInput = $("#textInput");
 
 $(function () {
 
-  var currentWeather = function (list) {
-    for (var result of list) {
-      console.log(result.temp);
-    };
-    $("#currentTemperature").text("Temp" + list.temp);
-    console.log(list.temp);
-  };
+  // var currentWeather = function (list) {
+  //   for (var result of Object.entries(list)) {
+  //     console.log(result.main.temp);
+  //   };
+  //   $("#currentTemperature").text("Temp" + result.temp);
+  //   console.log(result.temp);
+  // };
 
   var weatherResults = function (weather) {
-    
+
     var city = cityInput.val().trim();
     var apiKey = "06d1869c73d270a477b863d31ffacc7e"
     var tUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
@@ -27,14 +27,22 @@ $(function () {
         var lat = data.coord.lat;
         var lon = data.coord.lon;
         var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
+        
+        $("#currentCity").text(data.name);
+        $("#currentTemp").text("Temp:" + " " + data.main.temp + " °F");
+        $("#currentWind").text("Wind: " + " " + data.wind.speed + " MPH");
+        $("#currentHumidity").text("Humidity: " + " " + data.main.humidity + " %");
 
         fetch(weatherUrl)
           .then(function (response) {
             return response.json();
           })
-          .then(function (data) {
-            console.log(data);
-            currentWeather(data);
+          .then(function (weather) {
+            console.log(weather.list);
+            // $("#currentTemp").text("Temp:" + " " + weather.list[0].main.temp + " °F");
+            // $("#currentWind").text("Wind: " + " " + weather.list[0].wind.speed + " MPH");
+            // $("#currentHumidity").text("Humidity: " + " " + weather.list[0].main.humidity + " %");
+
           })
           .catch(function (error) {
             console.log(error);
