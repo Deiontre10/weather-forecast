088e07dd86499e6262a9678371d03fefab81fc25
Deiontre10 <travontazlowry@gmail.com>
2022-12-15 19:31:34 -0500

    got the data to return with the lat and lon of the search

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,33 +3,51 @@ var cityInput = $("#textInput");
 
 $(function () {
 
-  var currentWeather = function (main) {
-    // for (var result of main) {
-    //   console.log(result.main.temp);
-    // };
+  var currentWeather = function (list) {
+    for (var result of list) {
+      console.log(result.temp);
+    };
+    $("#currentTemperature").text("Temp" + list.temp);
+    console.log(list.temp);
   };
 
   var weatherResults = function (weather) {
-    // var lat = .coord.lat;
-    // var lon = .coord.lon;
+    
     var city = cityInput.val().trim();
     var apiKey = "06d1869c73d270a477b863d31ffacc7e"
-    // var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
-    var tUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=" + apiKey + "&units=imperial";
-    
+    var tUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
+
+
     fetch(tUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      currentWeather(data.main);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
+
+        fetch(weatherUrl)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            console.log(data);
+            currentWeather(data);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+
+
   };
-  
+
 
   searchBtn.on("click", function (event) {
     event.preventDefault();
@@ -41,6 +59,6 @@ $(function () {
     }
     var currentDate = dayjs().format("MM/DD/YYYY");
     $("#todaysDate").text(currentDate);
-    
+
   });
 });
